eee46ed2dea1b0dcbe962a6b5dfdde8939377713
qsebso <quinnsebso@gmail.com>
2023-11-28 15:55:12 -0700

    11/28/3:55pm

--- client.py
+++ client.py
@@ -1,2 +1,5 @@
-#This is our client.
-#What it needs to do is run when the computer turns on, turns off, and enters and exits sleep mode. Very simple#This is our client.
+
+# This is our client.
+# What it needs to do is run when the computer turns on, turns off,
+# and enters and exits sleep mode. Very simple This is our client.
+
