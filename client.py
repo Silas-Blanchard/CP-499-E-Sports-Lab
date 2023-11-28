9cbd0b232e24127dfff906e864115d70848a5fb0
qsebso <quinnsebso@gmail.com>
2023-11-28 15:58:16 -0700

    Hello World

--- client.py
+++ client.py
@@ -3,3 +3,6 @@
 # What it needs to do is run when the computer turns on, turns off,
 # and enters and exits sleep mode. Very simple This is our client.
 
+if __name__ == '__main__':
+    print("Hello World")
+
