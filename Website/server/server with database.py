a463af00f654112d380963849152ac4b51f48e1b
Silas Blanchard <silasb3435@gmail.com>
2024-01-28 22:55:52 -0700

    Chnage to server with database so it's agnostic too

--- Website/server/server with database.py
+++ Website/server/server with database.py
@@ -11,7 +11,7 @@ global_computer_status = 0
 # ======================== DATABASE STUFF ========================
 # connect to the local database
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(BASE_DIR, "html_and_layout_data/computer_status.db")
+db_path = os.path.join(BASE_DIR, "html_and_layout_data" , "computer_status.db")
 connection = sql.connect(db_path)
 
 # create cursor object to execute sqlite3 processes
