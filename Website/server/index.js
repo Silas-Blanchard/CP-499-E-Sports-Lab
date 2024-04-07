f7618d0d369f0cb18993c70e5927823ce687c2d9
qsebso <quinnsebso@gmail.com>
2024-04-07 17:53:41 -0600

    "Thinking it works need to push to live to see"

--- Website/server/index.js
+++ Website/server/index.js
@@ -1,51 +1,36 @@
-//the innumerable imports
 const express = require('express');
 const http = require('http');
 const { spawn } = require('node:child_process');
 const app = express();
 const server = http.createServer(app);
-const { Server } = require("socket.io")
-const {join} = require('path')
+const { Server } = require("socket.io");
+const { join } = require('path');
+const session = require('express-session');
+const { initializeCasAuth, checkWhitelist } = require('./content_gen/cas');
 
-const childPython = spawn('python3',['server/content_gen/six_website.py']);
-const childPythonJSON = spawn('python3',['server/content_gen/JSON-maker.py']);
+const childPython = spawn('python3', ['server/content_gen/six_website.py']);
+const childPythonJSON = spawn('python3', ['server/content_gen/JSON-maker.py']);
 
 const PORT = process.env.PORT || 3001;
-
-//var server  = app.listen(3001);
 const io = new Server(server);
 
-//sleep function that only works in async functions!
-//
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-//CAS!
-//import caspackage from 'cas-authentication';
-
-// const cas = new caspackage({
-//   cas_url: 'https://cc-cas.com/cas', //replace with the proper cas website, please
-//   service_url: 'http://esportscomm.coloradocollege.edu'
-// });
-
-//app.use(cas.bounce)
-
-//We will forever update our clients forever (or until they inevitably disconnect)
-async function notify(){
-  while (true){
+async function notify() {
+  while (true) {
     await sleep(5000)
-    spawn('python3',['server/content_gen/six_website.py']);
-    const hey = spawn('python3',['server/content_gen/JSON-maker.py']);
-    hey.stdout.on('data', function(data) {
-      var text = data.toString('utf8');// buffer to string
-      var str = text.replace(/'/g, '\"');//it does not like ' quotation marks it likes "
+    spawn('python3', ['server/content_gen/six_website.py']);
+    const hey = spawn('python3', ['server/content_gen/JSON-maker.py']);
+    hey.stdout.on('data', function (data) {
+      var text = data.toString('utf8');
+      var str = text.replace(/'/g, '\"');
       io.emit('update', str);
-  });
+    });
   }
 }
 
-notify()
+notify();
 
-//when our JSON maker goes off, we will have this print out the data. 
 childPythonJSON.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
 });
@@ -54,32 +39,46 @@ childPython.stderr.on('data', (data) => {
   console.log(`stderr: ${data}`);
 });
 
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Route to handle CAS authentication callback
+app.get('/admin', (req, res, next) => {
+  initializeCasAuth()(req, res, next);
+}, (req, res) => {
+  console.log('CAS authentication successful');
+  // After CAS authentication, check whitelist
+  checkWhitelist(req, res, () => {
+    // If user is on the whitelist, redirect to admin page
+    res.redirect('/admin');
+  }, () => {
+    // If user is not on the whitelist, redirect to main page
+    res.redirect('/');
+  });
+});
+
+// Main page route
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, '/html_and_layout_data/webber.html'));
 });
 
-app.get('/admin', (req, res) => {
-  res.sendFile(join(__dirname, '/html_and_layout_data/admin_page.html'));
-});
-
-// app.get('/admin', cas.block, (req, res) => {
-//   res.sendFile(join(__dirname, 'admin_page.html'));
-// });
-
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on("updateComps", function(data) {
+  socket.on("updateComps", function (data) {
     console.log(data)
-  })
+  });
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
 server.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
 
-childPython.on('close',(code) => {
+childPython.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
 });
