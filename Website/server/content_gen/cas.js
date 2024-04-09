bef0edf568421741757e75116a0c7ec86585c584
qsebso <quinnsebso@gmail.com>
2024-04-08 19:32:41 -0600

    yes

--- Website/server/content_gen/cas.js
+++ Website/server/content_gen/cas.js
@@ -15,9 +15,8 @@ function initializeCasAuth() {
 
 // Middleware to check whitelist
 function checkWhitelist(req, res, next) {
-  const userEmail = req.session.cas && req.session.cas.user;
   const adminWhitelist = ['s_blanchard@coloradocollege.edu', 'jlauer2023@coloradocollege.edu', 'q_sebso@gcoloradocollege.edu'];
-  if (userEmail && adminWhitelist.includes(userEmail)) {
+  if (adminWhitelist.includes(userEmail)) {
     // If user is on the whitelist, proceed to the next middleware
     res.redirect('/admin');
   } else {
